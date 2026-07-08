1cdaa447898472a650ceba90eb77c328e21a7de9
Valeriia Frolenko <valeriiiafrol@gmail.com>
2026-07-08 04:15:49 +0300

    add Category and Product entity classes with JPA annotations

---
+++ src/main/java/frolenko/supermarketweb/entity/Product.java
@@ -0,0 +1,23 @@
+package frolenko.supermarketweb.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idProduct;
+    private int categoryNumber;
+    private String manufacturer;
+    private String productName;
+    private String characteristics;
+}
