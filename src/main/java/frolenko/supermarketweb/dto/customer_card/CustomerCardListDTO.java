4be12002d708fae8a099349d9fb286cbb35343e1
Valeriia Frolenko <valeriiiafrol@gmail.com>
2026-07-12 07:35:54 +0300

    refactor CustomerCardListDTO and add CustomerCardQueryRepository with filtering capabilities

--- src/main/java/frolenko/supermarketweb/dto/customer_card/CustomerCardListDTO.java
+++ src/main/java/frolenko/supermarketweb/dto/customer_card/CustomerCardListDTO.java
@@ -5,9 +5,9 @@ import lombok.Builder;
 @Builder
 public record CustomerCardListDTO(
         String cardNumber,
-        String surname,
-        String name,
-        int discount,
-        String phone
+        String custSurname,
+        String custName,
+        int percent,
+        String phoneNumber
 ) {
 }
---
+++ src/main/java/frolenko/supermarketweb/query_repository/CustomerCardQueryRepository.java
@@ -0,0 +1,44 @@
+package frolenko.supermarketweb.query_repository;
+
+import frolenko.supermarketweb.dto.customer_card.CustomerCardListDTO;
+import frolenko.supermarketweb.filter.CustomerCardFilter;
+import frolenko.supermarketweb.utils.JooqConditionUtils;
+import lombok.RequiredArgsConstructor;
+import org.jooq.Condition;
+import org.jooq.DSLContext;
+import org.jooq.SortField;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static frolenko.generated.Tables.CUSTOMER_CARD;
+
+@Repository
+@RequiredArgsConstructor
+public class CustomerCardQueryRepository {
+
+    private final DSLContext dsl;
+
+    public List<CustomerCardListDTO> findByFilter(CustomerCardFilter filter) {
+        List<Condition> conditions = new ArrayList<>();
+        SortField<?> sortField = filter.getSortBy() != null
+                ? JooqConditionUtils.toSortField(filter.getSortBy(), filter.isAsc())
+                : CUSTOMER_CARD.CUST_SURNAME.asc();
+
+        JooqConditionUtils.addLikeIfNotNull(conditions, CUSTOMER_CARD.CUST_SURNAME, filter.getSurname());
+        JooqConditionUtils.addLikeIfNotNull(conditions, CUSTOMER_CARD.PHONE_NUMBER, filter.getPhoneNumber());
+        JooqConditionUtils.addRangeIfNotNull(conditions, CUSTOMER_CARD.PERCENT, filter.getDiscountFrom(), filter.getDiscountTo());
+
+        return dsl.select(
+                        CUSTOMER_CARD.CARD_NUMBER,
+                        CUSTOMER_CARD.CUST_SURNAME,
+                        CUSTOMER_CARD.CUST_NAME,
+                        CUSTOMER_CARD.PERCENT,
+                        CUSTOMER_CARD.PHONE_NUMBER)
+                .from(CUSTOMER_CARD)
+                .where(conditions)
+                .orderBy(sortField)
+                .fetchInto(CustomerCardListDTO.class);
+    }
+}
